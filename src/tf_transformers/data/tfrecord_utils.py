f81632f4bfaaa17c6181d9e61adafe8f49d323e3
legacyai <legacyai.org@gmail.com>
2021-02-22 15:16:57 +0530

    Added custom paded values option for tfrecord auto batch

--- src/tf_transformers/data/tfrecord_utils.py
+++ src/tf_transformers/data/tfrecord_utils.py
@@ -362,6 +362,7 @@ class TFReader(object):
         self,
         tf_dataset,
         batch_size,
+        padded_values=None,
         padded_shapes=None,
         x_keys=None,
         y_keys=None,
@@ -385,11 +386,18 @@ class TFReader(object):
             batched tf dataset
         """
         element_spec = tf_dataset.element_spec
-        padded_values = {}
+        _padded_values = {}
+        if not padded_values:
+            padded_values = {}
+        # sometimes we might have to have sme custom values other than 0
         for k, v in element_spec.items():
-            padded_values[k] = tf.constant(0, dtype=v.dtype)
+            if k in padded_values:
+                value = padded_values[k]
+                _padded_values[k] = tf.constant(value, dtype=value.dtype)
+            else:
+                _padded_values[k] = tf.constant(0, dtype=v.dtype)
         dataset = tf_dataset.padded_batch(
-            padding_values=padded_values,
+            padding_values=_padded_values,
             padded_shapes=padded_shapes,
             batch_size=batch_size,
             drop_remainder=drop_remainder,
