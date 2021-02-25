4331d233733797784c8565ed6d1dc795bb5ad2ea
legacyai <legacyai.org@gmail.com>
2021-02-25 13:54:55 +0000

    Added trainer back

--- src/tf_transformers/core/trainer.py
+++ src/tf_transformers/core/trainer.py
@@ -6,22 +6,9 @@ from tqdm import tqdm
 
 logging.set_verbosity("INFO")
 
-# Gradient Acculation credit goes to
-# Goran Sandstorm Nysater
 
-
-def SimpleTrainer(
-    model,
-    optimizer,
-    loss_fn,
-    dataset,
-    epochs,
-    num_train_examples,
-    batch_size,
-    steps_per_call=100,
-    gradient_accumulation_steps=1,
-):
-    """Simple trainer / Gradient accumulation steps
+def SimpleTrainer(model, optimizer, loss_fn, dataset, epochs, num_train_examples, batch_size, steps_per_call=100):
+    """Simple trainer
 
     Args:
         model ([type]): [description]
@@ -41,37 +28,23 @@ def SimpleTrainer(
     def train(iterator):
         """The step function for one training step"""
 
-        def reduce_grads(grads):
-            acc_grads = []
-            for var_grads in grads:
-                acc_grads.append(tf.reduce_mean(var_grads, axis=0))
-            return acc_grads
+        def train_step(batch_inputs, batch_labels):
+            """The computation to run on each TPU device."""
+            with tf.GradientTape() as tape:
+                model_outputs = model(batch_inputs)
+                loss = loss_fn(batch_labels, model_outputs)
 
-        def train_step(iterator):
-            grads = [tf.TensorArray(tf.float32, size=steps) for v in model.trainable_variables]
-            for i in tf.range(tf.convert_to_tensor(gradient_accumulation_steps)):
-                batch_inputs, batch_labels = next(iterator)
-                with tf.GradientTape() as tape:
-                    model_outputs = model(batch_inputs)
-                    loss = loss_fn(batch_labels, model_outputs)
-                # training_loss.update_state(loss * strategy.num_replicas_in_sync)
-                training_loss.update_state(loss)
-
-                grads = tape.gradient(loss, model.trainable_variables)
-                for j, g in enumerate(grads):
-                    grads[j].write(i, g)
-            grads_reduced = reduce_grads([grad_arr.stack() for grad_arr in grads])
-            optimizer.apply_gradients(zip(grads_reduced, model.trainable_variables))
-            # Account optimizer for global_batch steps
-            optimizer.iterations.assign_add(gradient_accumulation_steps - 1)
+            grads = tape.gradient(loss, model.variables)
+            optimizer.apply_gradients(zip(grads, model.variables))
+            # training_loss.update_state(loss * strategy.num_replicas_in_sync)
+            training_loss.update_state(loss)
 
         for _ in tf.range(tf.convert_to_tensor(steps_per_call)):
+            batch_inputs, batch_labels = next(iterator)
             train_step(batch_inputs, batch_labels)
 
     # Make dataset iterator
 
-    # To make it properly divisible
-    steps_per_call = steps_per_call // gradient_accumulation_steps
     GLOBAL_STEP = epochs * (num_train_examples // (batch_size * steps_per_call))
     logging.info("Global Steps {}".format(GLOBAL_STEP))
     iter_dataset = iter(dataset)
