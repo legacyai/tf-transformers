f36f9c71e1fad78c92ed0d83603ec790feb69d1e
legacyai <legacyai.org@gmail.com>
2021-03-02 14:15:54 +0530

    Added gradient accumulation

--- src/tf_transformers/core/trainer.py
+++ src/tf_transformers/core/trainer.py
@@ -7,7 +7,68 @@ from tqdm import tqdm
 logging.set_verbosity("INFO")
 
 
-def SimpleTrainer(
+def Trainer(model, optimizer, loss_fn, dataset, epochs, num_train_examples, batch_size, steps_per_call=100):
+    """Simple trainer
+
+    Args:
+        model ([type]): [description]
+        optimizer ([type]): [description]
+        loss ([type]): [description]
+        dataset ([type]): [description]
+        epochs ([type]): [description]
+        num_train_examples ([type]): [description]
+        batch_size ([type]): [description]
+        steps_per_call (int, optional): [description]. Defaults to 100.
+
+    Returns:
+        [type]: [description]
+    """
+
+    @tf.function
+    def train(iterator):
+        """The step function for one training step"""
+
+        def train_step(batch_inputs, batch_labels):
+            """The computation to run on each TPU device."""
+            with tf.GradientTape() as tape:
+                model_outputs = model(batch_inputs)
+                loss = loss_fn(batch_labels, model_outputs)
+
+            grads = tape.gradient(loss, model.variables)
+            optimizer.apply_gradients(zip(grads, model.variables))
+            # training_loss.update_state(loss * strategy.num_replicas_in_sync)
+            training_loss.update_state(loss)
+
+        for _ in tf.range(tf.convert_to_tensor(steps_per_call)):
+            batch_inputs, batch_labels = next(iterator)
+            train_step(batch_inputs, batch_labels)
+
+    # Make dataset iterator
+
+    GLOBAL_STEP = epochs * (num_train_examples // (batch_size * steps_per_call))
+    logging.info("Global Steps {}".format(GLOBAL_STEP))
+    iter_dataset = iter(dataset)
+    training_loss = tf.keras.metrics.Mean(
+        "training_loss", dtype=tf.float32
+    )  # We store loss here and reset after every global steps
+
+    loss_holder = []
+    for step_iter in tqdm(range(GLOBAL_STEP)):
+        start_time = time.time()
+        train(iter_dataset)
+        end_time = time.time()
+        loss_holder.append(training_loss.result())
+        logging.info(
+            "Global step {}, time {} seconds, loss {}".format(
+                step_iter, round(end_time - start_time, 4), loss_holder[-1]
+            )
+        )
+        training_loss.reset_states()
+
+    return loss_holder
+
+
+def SimpleTrainerGradientAccumulation(
     model,
     optimizer,
     loss_fn,
@@ -54,27 +115,31 @@ def SimpleTrainer(
             inputs = next(iterator)
             grads, loss = get_gradient(inputs)
             training_loss.update_state(loss)
-            grads_holder = [tf.TensorArray(tf.float32, size=1) for v in model.trainable_variables]
-            trainable_variables = []
+            grads_holder = [
+                tf.TensorArray(tf.float32, size=1, dynamic_size=True, infer_shape=False, element_shape=v.shape)
+                for v in model.trainable_variables
+            ]
             for (j, g) in enumerate(grads):
                 if g is not None:
-                    grads_holder[j].write(0, g)
-                    trainable_variables.append(model.trainable_variables[j])
+                    grads_holder[j] = grads_holder[j].write(0, g)
+                else:
+                    grads_holder[j] = grads_holder[j].write(0, tf.zeros_like(model.trainable_variables[j]))
 
-            for i in tf.range(gradient_accumulation_steps - 1):
+            # Further iterations
+            for i in range(gradient_accumulation_steps - 1):
                 inputs = next(iterator)
                 grads, loss = get_gradient(inputs)
                 training_loss.update_state(loss)
                 for (j, g) in enumerate(grads):
                     if g is not None:
                         new_tensor = grads_holder[j].read(0) + g
-                        grads_holder[j].write(0, new_tensor)
+                        grads_holder[j] = grads_holder[j].write(0, new_tensor)
 
-            # Delete unwanted (grads which are None)
-            for (j, g) in enumerate(grads):
-                if g is not None:
-                    del grads_holder[j]
-            optimizer.apply_gradients(zip(grads, trainable_variables))
+            # Unstack grads holder
+            grads_holder = [
+                tf.squeeze(grad_arr.stack(), axis=0) / gradient_accumulation_steps for grad_arr in grads_holder
+            ]
+            optimizer.apply_gradients(zip(grads_holder, model.trainable_variables))
             optimizer.iterations.assign_add(gradient_accumulation_steps - 1)
             # training_loss.update_state(loss * strategy.num_replicas_in_sync)
 
@@ -107,3 +172,30 @@ def SimpleTrainer(
         training_loss.reset_states()
 
     return loss_holder
+
+
+def SimpleTrainer(
+    model,
+    optimizer,
+    loss_fn,
+    dataset,
+    epochs,
+    num_train_examples,
+    batch_size,
+    steps_per_call=100,
+    gradient_accumulation_steps=None,
+):
+    if gradient_accumulation_steps:
+        SimpleTrainerGradientAccumulation(
+            model,
+            optimizer,
+            loss_fn,
+            dataset,
+            epochs,
+            num_train_examples,
+            batch_size,
+            steps_per_call,
+            gradient_accumulation_steps,
+        )
+    else:
+        Trainer(model, optimizer, loss_fn, dataset, epochs, num_train_examples, batch_size, steps_per_call)
