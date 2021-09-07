e10c42ebe4f35e357ee8ab0f18ce06716d0bfc01
R Nair <a642163@indemacl321174.fmr.com>
2021-09-07 13:55:31 +0530

    Added Albert details

--- src/tf_transformers/models/albert/albert.py
+++ src/tf_transformers/models/albert/albert.py
@@ -1,3 +1,21 @@
+# coding=utf-8
+# Copyright 2021 TF-Transformers Authors.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""TF 2.0 Albert Model"""
+
 import tensorflow as tf
 from absl import logging
 
@@ -12,16 +30,14 @@ logging.set_verbosity("INFO")
 
 
 class AlbertEncoder(LegacyLayer):
-    """BERT based encoder / Decoder .
-    BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding
-    Authors: Jacob Devlin, Ming-Wei Chang, Kenton Lee, Kristina Toutanova
+    """Albert based encoder / Decoder .
+    ALBERT: A Lite BERT for Self-supervised Learning of Language Representations
+    Authors: Zhenzhong Lan, Mingda Chen, Sebastian Goodman, Kevin Gimpel, Piyush Sharma, Radu Soricut
+    
+    Implementation of Albert in TF2.0
 
-    Implementation of Bert in TF2.0
-
-    Paper: https://arxiv.org/abs/1810.04805
-    Official Code: https://github.com/google-research/bert
-
-
+    Paper: https://arxiv.org/abs/1909.11942
+    Official Code: https://github.com/google-research/albert
     """
 
     def __init__(
