fe7fa31ca4a1e6f5320e019c66646f3fa6687dbe
Sarath R Nair <sarathrnair@Saraths-MacBook-Pro.local>
2021-12-06 12:45:30 +0530

    Added labels smoothing bf16 supported

--- src/tf_transformers/losses/cross_entropy.py
+++ src/tf_transformers/losses/cross_entropy.py
@@ -77,18 +77,21 @@ def cross_entropy_loss_label_smoothing(labels, logits, smoothing=0.1, label_weig
     vocab_float = tf.cast(vocab_size - 1, tf.float32)
     low_confidence = (1.0 - confidence) / vocab_float
     soft_targets = tf.one_hot(labels, depth=vocab_size, on_value=confidence, off_value=low_confidence)
-    xentropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=soft_targets)
+    per_example_loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=soft_targets)
     # Calculate the best (lowest) possible value of cross entropy, and
     # subtract from the cross entropy loss.
     normalizing_constant = -(
         confidence * tf.math.log(confidence) + vocab_float * low_confidence * tf.math.log(low_confidence + 1e-20)
     )
-    xentropy = xentropy - tf.cast(normalizing_constant, xentropy.dtype)
-    # xentropy = tf.cast(xentropy, tf.float32)
+    per_example_loss = per_example_loss - tf.cast(normalizing_constant, per_example_loss.dtype)
+    per_example_loss = tf.cast(per_example_loss, tf.float32)
+
     if label_weights is None:
         label_weights = tf.ones_like(labels)
     label_weights = tf.cast(label_weights, tf.float32)
-    per_example_loss = xentropy * label_weights
+    per_example_loss = per_example_loss * label_weights
+    # Take mean along axis=1 and then divide that first
+    # Otherwise float16 will overflow
     numerator = tf.reduce_sum(per_example_loss, axis=1)
     denominator = tf.reduce_sum(label_weights, axis=1)
     loss = tf.math.divide_no_nan(numerator, denominator)
