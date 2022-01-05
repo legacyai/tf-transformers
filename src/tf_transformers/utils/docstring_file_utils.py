4e2f08b5ed59dcb84da8a1020a6d5a6d0445c6e4
legacyai <67182839+legacyai@users.noreply.github.com>
2022-01-05 10:09:58 +0000

    Deploying to gh-pages from @ legacyai/tf-transformers@d25690c3cba5a8332498bdf40910b9f07f59b566 🚀

--- src/tf_transformers/utils/docstring_file_utils.py
+++
@@ -1,6 +0,0 @@
-def add_start_docstrings(*docstr):
-    def docstring_decorator(fn):
-        fn.__doc__ = "".join(docstr) + (fn.__doc__ if fn.__doc__ is not None else "")
-        return fn
-
-    return docstring_decorator
