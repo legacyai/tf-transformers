9290df07dc58e9162eb88cc7cc075989d7453b7f
legacyai <legacyai.org@gmail.com>
2022-01-12 12:26:05 +0530

    fix: Adding workflow for release

--- patch_version.py
+++ patch_version.py
@@ -1,38 +1,40 @@
 # This script is adapted from python-semantic-release
 
+import sys
+
 from absl import logging
 from semantic_release import cli
 
 logging.set_verbosity("DEBUG")
 
 
-def push_version(retry=False, noop=False, force_level=None, **kwargs):
+def push_version(current_version, retry=False, noop=False, force_level=None, **kwargs):
     """
     Detect the new version according to git log and semver.
     Write the new version number and commit it, unless the noop option is True.
     """
 
-    if retry:
-        logging.info("Retrying publication of the same version")
-    else:
-        logging.info("Creating new version")
+    # if retry:
+    #     logging.info("Retrying publication of the same version")
+    # else:
+    #     logging.info("Creating new version")
 
-    # Get the current version number
-    try:
-        current_version = cli.get_current_version()
-        logging.info(f"Current version: {current_version}")
-    except cli.GitError as e:
-        logging.error(str(e))
-        return False
+    # # Get the current version number
+    # try:
+    #     current_version = cli.get_current_version()
+    #     logging.info(f"Current version: {current_version}")
+    # except cli.GitError as e:
+    #     logging.error(str(e))
+    #     return False
 
-    if not cli.should_bump_version(
-        current_version=current_version, new_version=current_version, retry=retry, noop=noop
-    ):
-        return False
+    # if not cli.should_bump_version(
+    #     current_version=current_version, new_version=current_version, retry=retry, noop=noop
+    # ):
+    #     return False
 
-    if retry:
-        # No need to make changes to the repo, we're just retrying.
-        return True
+    # if retry:
+    #     # No need to make changes to the repo, we're just retrying.
+    #     return True
 
     # Bump the version
     cli.bump_version(current_version, level_bump='dummy')
@@ -40,4 +42,5 @@ def push_version(retry=False, noop=False, force_level=None, **kwargs):
 
 
 if __name__ == '__main__':
-    push_version()
+    version = sys.argv[-1]  # last cli argument
+    push_version(version)
