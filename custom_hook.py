4a813e269562a0589e25a8864ea03261520708ee
Sarath R Nair <legacyai.org@gmail.com>
2022-03-11 07:08:01 +0530

    Added MLM TPU tutorial

--- custom_hook.py
+++ custom_hook.py
@@ -16,14 +16,14 @@ def add_new_files_to_jupytext():
         if notebook_name not in all_md:
             notebook_list.append(notebook_name + '.ipynb')
 
-    subprocess.run(["jupytext --help"], shell=True)
     if notebook_list != []:
         for notebook in notebook_list:
             subprocess.run(["jupytext --set-formats ipynb,md:myst {}".format(notebook)], shell=True)
 
 
 def move_to_docs():
-    "Move tutorals to docs"
+    """Move tutorals to docs"""
+    print("Copying tutorials to docs")
     import shutil
 
     shutil.copytree("tutorials", "docs/source/tutorials", dirs_exist_ok=True)
